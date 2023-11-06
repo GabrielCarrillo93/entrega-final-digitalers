2ed84f7cadea3d8bd96c33457b70c709ffa0f8a8
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2023-11-05 21:53:11 -0300

    hot fix 9

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,8 @@ import Skills from './components/Skills/Skills'
 
 function App() {
     const [proyectos, setProyectos] = useState("responsive");
-    const [recarga, setRecarga] = useState(1)
 
-    //tuve que hacer esto para forzar que se vea el carrousel en mobile
-    setTimeout(() =>{
-        setRecarga(recarga + 1)
-    }, 100)
+    
     
     useEffect(() => {
       console.log("hola");
